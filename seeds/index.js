02c80beada468a0caae20a6371ab026ebd226c45
Eric Van Meter <ericvanmeter57@gmail.com>
2021-05-11 22:06:36 -0500

    image upload feature finished

--- seeds/index.js
+++ seeds/index.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers')
+const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/marshmellow-trail', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
@@ -18,18 +18,28 @@ db.once("open", () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
+
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '6099779a161b609f24235766',
+            author: '609b4218fd0c5931e4487272',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/outbounderic/image/upload/v1620785847/MarshmellowTrail/camping_tents.0_iwxzsm.jpg',
+                    filename: 'MarshmellowTrail/ahfnenvca4tha00h2ubt'
+                },
+                {
+                    url: 'https://res.cloudinary.com/outbounderic/image/upload/v1620785858/MarshmellowTrail/camping_tents.3_flngpk.jpg',
+                    filename: 'MarshmellowTrail/ruyoaxgf72nzpi4y6cdi'
+                }
+            ]
         })
         await camp.save();
     }
