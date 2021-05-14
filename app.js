c0c0ac645b4ec259deaa4802f60177d72e9a822f
Eric Van Meter <ericvanmeter57@gmail.com>
2021-05-14 18:00:20 -0500

    adding mongodb atlas

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-if (process.env.NODE_ENV !== "production") {
+  if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
-
 
 const express = require('express');
 const path = require('path');
@@ -15,16 +14,17 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const helmet = require('helmet');
-
 const mongoSanitize = require('express-mongo-sanitize');
-
-
-
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/marshmellow-trail', {
+const MongoStore = require("connect-mongo");
+
+// const dbURL = process.env.DB_URL ||;
+const dbUrl = 'mongodb://localhost:27017/marshmellow-trail'
+
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -50,7 +50,22 @@ app.use(mongoSanitize({
     replaceWith: '_'
 }))
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: 'thisshouldbeabettersecret!'
+    }
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
 const sessionConfig = {
+    store,
     name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
